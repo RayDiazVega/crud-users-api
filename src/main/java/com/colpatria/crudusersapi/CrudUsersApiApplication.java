b5ba5d8c7713ff01c8e90f4ffc294c53cdda01e9
Raymundo Diaz Vega <rvega@excelcredit.co>
2022-05-24 22:10:14 -0500

    Added service

---
+++ src/main/java/com/colpatria/crudusersapi/CrudUsersApiApplication.java
@@ -0,0 +1,13 @@
+package com.colpatria.crudusersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudUsersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudUsersApiApplication.class, args);
+	}
+
+}
