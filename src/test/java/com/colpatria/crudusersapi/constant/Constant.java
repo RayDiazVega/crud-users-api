5471a39198c1f3e7fc14cee20a7397acea961682
Raymundo Diaz Vega <rvega@excelcredit.co>
2022-05-26 02:25:18 -0500

    Added user tests

---
+++ src/test/java/com/colpatria/crudusersapi/constant/Constant.java
@@ -0,0 +1,12 @@
+package com.colpatria.crudusersapi.constant;
+
+public class Constant {
+
+  public static final String user = "{\n"
+      + "  \"id\": 1,\n"
+      + "  \"names\": \"Poracius Baudato\",\n"
+      + "  \"surnames\": \"Yettengia Kenicillate\",\n"
+      + "  \"email\": \"poraciusyettengia@imgur.com\",\n"
+      + "  \"tasks\": []\n"
+      + "}";
+}
---
+++ src/test/java/com/colpatria/crudusersapi/user/infrastructure/adapters/UserControllerTest.java
@@ -0,0 +1,134 @@
+package com.colpatria.crudusersapi.user.infrastructure.adapters;
+
+import com.colpatria.crudusersapi.constant.Constant;
+import java.time.LocalDate;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestMethodOrder(OrderAnnotation.class)
+class UserControllerTest {
+
+  @Autowired
+  MockMvc mockMvc;
+
+  String user = Constant.user;
+
+  @Test
+  @Order(1)
+  void saveStatus200() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.post("/user")
+            .contentType(MediaType.APPLICATION_JSON).content(user))
+        .andExpectAll(MockMvcResultMatchers.status().isOk(),
+            MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON),
+            MockMvcResultMatchers.jsonPath("$.id").isNumber(),
+            MockMvcResultMatchers.jsonPath("$.email").isString())
+        .andDo(MockMvcResultHandlers.print());
+  }
+
+  @Test
+  @Order(1)
+  void saveStatus400() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.post("/user")
+            .contentType(MediaType.APPLICATION_JSON).content(user))
+        .andExpectAll(MockMvcResultMatchers.status().isBadRequest(),
+            MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON),
+            MockMvcResultMatchers.jsonPath("$.error").value("¡User email already exists!"),
+            MockMvcResultMatchers.jsonPath("$.message").value("Client error"))
+        .andDo(MockMvcResultHandlers.print());
+
+    user = user.replace("Poracius", "1");
+
+    mockMvc.perform(MockMvcRequestBuilders.post("/user")
+            .contentType(MediaType.APPLICATION_JSON).content(user))
+        .andExpectAll(MockMvcResultMatchers.status().isBadRequest(),
+            MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON),
+            MockMvcResultMatchers.jsonPath("$.error.[0]").value("names must be a valid value"),
+            MockMvcResultMatchers.jsonPath("$.message").value("Client error"))
+        .andDo(MockMvcResultHandlers.print());
+  }
+
+  @Test
+  @Order(2)
+  void findAll() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.get("/user"))
+        .andExpectAll(MockMvcResultMatchers.status().isOk(),
+            MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON),
+            MockMvcResultMatchers.jsonPath("$.totalElements").isNumber(),
+            MockMvcResultMatchers.jsonPath("$.content").isArray())
+        .andDo(MockMvcResultHandlers.print());
+  }
+
+  @Test
+  @Order(3)
+  void findById() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.get("/user/1"))
+        .andExpectAll(MockMvcResultMatchers.status().isOk(),
+            MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON),
+            MockMvcResultMatchers.jsonPath("$.id").isNumber(),
+            MockMvcResultMatchers.jsonPath("$.email").isString())
+        .andDo(MockMvcResultHandlers.print());
+  }
+
+  @Test
+  @Order(4)
+  void findByEmail() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.get("/user/email/poraciusyettengia@imgur.com"))
+        .andExpectAll(MockMvcResultMatchers.status().isOk(),
+            MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON),
+            MockMvcResultMatchers.jsonPath("$.id").isNumber(),
+            MockMvcResultMatchers.jsonPath("$.email").isString())
+        .andDo(MockMvcResultHandlers.print());
+  }
+
+  @Test
+  @Order(5)
+  void findByCreatedDateBetween() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.get("/user/createdDate/{from}/{to}",
+            LocalDate.now() + "T00:00:00", LocalDate.now() + "T23:59:59"))
+        .andExpectAll(MockMvcResultMatchers.status().isOk(),
+            MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON),
+            MockMvcResultMatchers.jsonPath("$.[0].id").isNumber(),
+            MockMvcResultMatchers.jsonPath("$.[0].email").isString())
+        .andDo(MockMvcResultHandlers.print());
+  }
+
+  @Test
+  @Order(6)
+  void update() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.put("/user")
+            .contentType(MediaType.APPLICATION_JSON).content(user))
+        .andExpectAll(MockMvcResultMatchers.status().isOk(),
+            MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON),
+            MockMvcResultMatchers.jsonPath("$.id").isNumber(),
+            MockMvcResultMatchers.jsonPath("$.email").isString())
+        .andDo(MockMvcResultHandlers.print());
+  }
+
+  @Test
+  @Order(7)
+  void deleteById() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.delete("/user/1"))
+        .andExpectAll(MockMvcResultMatchers.status().isOk())
+        .andDo(MockMvcResultHandlers.print());
+  }
+
+  @Test
+  @Order(8)
+  void deleteAll() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.delete("/user"))
+        .andExpectAll(MockMvcResultMatchers.status().isOk())
+        .andDo(MockMvcResultHandlers.print());
+  }
+}
