6090f5b6fa9c244073300a5ba9266b94dd4740db
Raymundo Diaz Vega <rvega@excelcredit.co>
2022-05-26 02:19:07 -0500

    Added jacoco plugin

--- src/test/java/com/colpatria/crudusersapi/CrudUsersApiApplicationTests.java
+++ src/test/java/com/colpatria/crudusersapi/CrudUsersApiApplicationTests.java
@@ -1,13 +1,35 @@
 package com.colpatria.crudusersapi;
 
+import com.colpatria.crudusersapi.user.application.TaskService;
+import com.colpatria.crudusersapi.user.application.UserService;
+import com.colpatria.crudusersapi.user.infrastructure.adapters.TaskController;
+import com.colpatria.crudusersapi.user.infrastructure.adapters.UserController;
+import com.colpatria.crudusersapi.user.infrastructure.ports.TaskRepository;
+import com.colpatria.crudusersapi.user.infrastructure.ports.UserRepository;
+import lombok.AllArgsConstructor;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AllArgsConstructor(onConstructor_ = {@Autowired})
 class CrudUsersApiApplicationTests {
+
+	TaskRepository taskRepository;
+	TaskService taskService;
+	TaskController taskController;
+	UserRepository userRepository;
+	UserService userService;
+	UserController userController;
 
 	@Test
 	void contextLoads() {
+		Assertions.assertNotNull(taskRepository);
+		Assertions.assertNotNull(taskService);
+		Assertions.assertNotNull(taskController);
+		Assertions.assertNotNull(userRepository);
+		Assertions.assertNotNull(userService);
+		Assertions.assertNotNull(userController);
 	}
-
 }
